6f0c2cff1e837853cfc9100f495bc65d6622cdcd
Kartik Srivastava <69189503+kartikkodes@users.noreply.github.com>
2021-04-25 15:42:41 +0530

    List and its use

---
+++ list.py
@@ -0,0 +1,21 @@
+digits = [0,1,2,3,4,5,6,7,8,9]
+# print(digits)
+# print(digits[0])
+# print(digits[-10])
+# print(digits[-1])
+
+# print(len(digits))
+# print(digits[-len(digits)]) 
+# print(digits[:-1])
+# print(digits[0:10:2]) 
+# print(digits[::-1]) #It will reverse the list as it will hop backward
+# print(digits[::-2])
+
+# for i in range(len(digits)):
+#     print(digits[0:i]) #from 0 to length iteratively
+
+
+for i in range(len(digits)):
+    print(digits[i:i+3])
+
+
