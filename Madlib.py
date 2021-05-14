db7e6995703d91e8d315bb03ed6b1f605c15f127
Kartik Srivastava <69189503+kartikkodes@users.noreply.github.com>
2021-05-14 20:01:30 +0530

    Madlib

---
+++ Madlib.py
@@ -0,0 +1,8 @@
+adjective = input("Input adjctive")
+verb2 = input("Input verb2")
+verb1 = input("Input verb1")
+obj1 = input("Inout object1: ")
+
+madlib = f"The party began at 4 p.m. For about one hour, we ate to our heart’s {verb1}, {verb1} and {verb2} without {obj1}. The party came to an end at 5 p.m."
+
+print(madlib)            
